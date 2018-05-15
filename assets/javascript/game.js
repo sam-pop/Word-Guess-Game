91f43df28ad3f8f994e40f31bd64d3686c27d8b9
sam-pop <poplovitch.samuel@gmail.com>
2018-05-15 11:57:00 -0400

    functions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,28 @@ var pastLetters = []; // letter already used by the player
 var numOfGuessesLeft; // number of guess left for the player 
 var numOfWins = []; // total number of player wins this round
 
-//checks if the arg is a letter (returns: true or error msg for the user)
+// checks if the arg is a letter (returns: true or error msg for the user)
 function isLetter(str) {
     if (str.length === 1 && str.match(/[a-zA-Z]/i))
         return true;
-    else return "Please input only letters!";
+    else return "Please input letters only!";
 }
 
-//adds a win to the total wins tally
+// adds a win to the total wins tally
 function addWin() {
     this.numOfWins++;
+}
+
+// decrement the number of guesses left
+function decGuesses() {
+    this.numOfGuessesLeft--;
+}
+
+function splitIntoLetters(word) {
+    currentWord = word.split('');
+}
+
+//TODO: runs the game!
+function runGame() {
+
 }
