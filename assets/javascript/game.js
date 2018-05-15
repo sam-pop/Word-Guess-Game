47adff7b8d8c6515430610493d220bdcc26810b2
sam-pop <poplovitch.samuel@gmail.com>
2018-05-15 11:43:08 -0400

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,19 @@
 var wordDB = []; // the DB of words to pick from
 var randIndex; // random value to be used as an index to pull a random word from DB
 var currentWord = []; // holds the current word
-var numOfGuessesLeft; // number of guess left for the player 
+var currentLetter; // holds the current user input
 var pastLetters = []; // letter already used by the player
+var numOfGuessesLeft; // number of guess left for the player 
 var numOfWins = []; // total number of player wins this round
+
+//checks if the arg is a letter (returns: true or error msg for the user)
+function isLetter(str) {
+    if (str.length === 1 && str.match(/[a-zA-Z]/i))
+        return true;
+    else return "Please input only letters!";
+}
+
+//adds a win to the total wins tally
+function addWin() {
+    this.numOfWins++;
+}
